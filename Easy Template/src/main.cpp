afb6b76e4cf075fceb53ff88438c731275ced47a
Emma Li <Emma.lipishan@gmail.com>
2024-11-24 10:45:15 -0700

    standardsplitarcade

--- Easy Template/src/main.cpp
+++ Easy Template/src/main.cpp
@@ -139,9 +139,9 @@ void opcontrol() {
     }
 
     // chassis.opcontrol_tank();  // Tank control
-    //chassis.opcontrol_arcade_standard(ez::SPLIT);   // Standard split arcade
+    chassis.opcontrol_arcade_standard(ez::SPLIT);   // Standard split arcade
     // chassis.opcontrol_arcade_standard(ez::SINGLE);  // Standard single arcade
-    chassis.opcontrol_arcade_flipped(ez::SPLIT);    // Flipped split arcade
+    //chassis.opcontrol_arcade_flipped(ez::SPLIT);    // Flipped split arcade
     // chassis.opcontrol_arcade_flipped(ez::SINGLE);   // Flipped single arcade
 
     // . . .
