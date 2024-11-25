8d5c2f3356084d6e402a611dbaf98e5588446bdf
Emma Li <Emma.lipishan@gmail.com>
2024-11-24 21:32:45 -0700

    3.5curve

--- Easy Template/src/main.cpp
+++ Easy Template/src/main.cpp
@@ -30,9 +30,9 @@ void initialize() {
   // Configure your chassis controls
   chassis.opcontrol_curve_buttons_toggle(true);  // Enables modifying the controller curve with buttons on the joysticks
   chassis.opcontrol_drive_activebrake_set(0);    // Sets the active brake kP. We recommend ~2.  0 will disable.
-  chassis.opcontrol_curve_default_set(2.6, 2.6);     // Defaults for curve. If using tank, only the first parameter is used. (Comment this line out if you have an SD card!)
+  chassis.opcontrol_curve_default_set(3.5, 3.5);     // Defaults for curve. If using tank, only the first parameter is used. (Comment this line out if you have an SD card!)
 
-  // Set the drive to your own constants from autons.cpp!
+  // Set the drive to your own constants from autons.cpp
   default_constants();
 
   // These are already defaulted to these buttons, but you can change the left/right curve buttons here!
