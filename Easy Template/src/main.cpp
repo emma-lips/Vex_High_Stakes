f50970f87b3cb1b766bef4d3993f2ab86f61865b
Emma Li <Emma.lipishan@gmail.com>
2025-01-12 22:20:54 -0700

    Motor speed

--- Easy Template/src/main.cpp
+++ Easy Template/src/main.cpp
@@ -13,7 +13,7 @@ ez::Drive chassis(
 
     7,      // IMU Port
     2.75,  // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
-    450);   // Wheel RPM
+    375);   // Wheel RPM
 
     // right motors are 15, 14, 11
     // left motors are 2, 5, 3
@@ -40,7 +40,7 @@ void initialize() {
   // Configure your chassis controls
   chassis.opcontrol_curve_buttons_toggle(true);  // Enables modifying the controller curve with buttons on the joysticks
   chassis.opcontrol_drive_activebrake_set(0);    // Sets the active brake kP. We recommend ~2.  0 will disable.
-  chassis.opcontrol_curve_default_set(3, 3);     // Defaults for curve. If using tank, only the first parameter is used. (Comment this line out if you have an SD card!)
+  chassis.opcontrol_curve_default_set(2.4, 2.4);     // Defaults for curve. If using tank, only the first parameter is used. (Comment this line out if you have an SD card!)
 
   // Set the drive to your own constants from autons.cpp
   default_constants();
