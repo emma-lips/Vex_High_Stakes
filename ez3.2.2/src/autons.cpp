1d5f9083088412fe0bf43aa90640634f529e6f81
Emma Li <Emma.lipishan@gmail.com>
2025-02-08 12:06:09 -0700

    changingdelay_2tointake1stdonut

--- ez3.2.2/src/autons.cpp
+++ ez3.2.2/src/autons.cpp
@@ -24,7 +24,7 @@ const double RIGHTblueturn = -30; // turn towards mogo
 const double RIGHTblueback2 = -15; // / move to mogo (group of 8)
 const double RIGHTblueturn2 = -95; //Turning to direction of first donut (preload does not count)
 const double RIGHTblueforward = 23; // move to first donut
-const int delay_2 = 1000; // intaking and dropping first donut
+const int delay_2 = 2000; // intaking and dropping first donut
 
 //For donut side(right blue(stack of 8 donuts)) 
 const double RIGHTblueturn3 = -175; // turn towards second donut
@@ -725,7 +725,7 @@ void weakahhblueleft() {
 
   chassis.pid_turn_set(-90, TURN_SPEED);
   chassis.pid_wait_quick_chain();
-  chassis.pid_drive_set(13, DRIVE_SPEED);
+  chassis.pid_drive_set(23, DRIVE_SPEED);
   chassis.pid_wait_quick_chain();
   chassis.pid_drive_set(4, DRIVE_SPEED);
 
