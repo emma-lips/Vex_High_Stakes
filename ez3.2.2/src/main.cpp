d022d9f9ea4f6db9790cfad2379c7f63bff1bb5a
Emma Li <Emma.lipishan@gmail.com>
2025-05-06 18:54:19 -0600

    right blue gets 1+5 but no time to 1+6

--- ez3.2.2/src/main.cpp
+++ ez3.2.2/src/main.cpp
@@ -160,12 +160,13 @@ void initialize() {
 
   // Autonomous Selector using LLEMU
 ez::as::auton_selector.autons_add({
+  Auton("worlds elim auton right blue \n\nrightblue 1+6", worldssixdonutsrightblueelims),
   Auton("worlds elim auton left blue \n\nleft blue 1+4 corner clear", worldssixdonutsleftblueelims),
   Auton("worlds elim auton right red \n\nrightred 1+4 corner clear", worldssixdonutsrightredelims),
 
 
   Auton("worlds elim auton left red \n\nleftred 1+6", worldssixdonutsleftredelims),
-  Auton("worlds elim auton right blue \n\nrightblue 1+6", worldssixdonutsrightblueelims),
+
   Auton("worlds auton left blue alliance 4 donuts \n\nleftblue 1 + 4 touch ladder", worldssixdonutsleftbluealliancetransposed),
   Auton("worlds auton right red alliance stake 4 donuts \n\nrightred 1+4 touch ladder", worldssixdonutsrightredalliance),
   Auton("worlds auton right blue alliance stake 5 donuts \n\nrightblue 1+5 touch ladder", worldssixdonutsrightblue),
